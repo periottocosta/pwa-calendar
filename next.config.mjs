8f3a62d0e3a32dfec6e2c404ba4020d33b49f861
periottocosta <gabrielperiotto@hotmail.com>
2024-09-30 21:36:27 -0300

    feat: first updates on project structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+import path from 'path';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development"
+    },
+    webpack(config) {
+        config.module.rules.forEach((rule) => {
+            const { oneOf } = rule;
+            if (oneOf) {
+                oneOf.forEach((one) => {
+                    if (!`${one.issuer?.and}`.includes('_app')) return;
+                    one.issuer.and = [path.resolve(path.resolve('./'))];
+                });
+            }
+        })
+        return config;
+    },
+};
+
+export default withPWA({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+})(nextConfig);
