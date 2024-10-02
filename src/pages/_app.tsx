653c774ec6a10993b79fdc731fd63c957a2906cc
periottocosta <gabrielperiotto@hotmail.com>
2024-10-02 17:54:38 -0300

    build: update project structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
+import Head from 'next/head';
+
+import "@/styles/styles.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png"></link>
+                <meta name="theme-color" content="#red" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Holiday Calendar</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
